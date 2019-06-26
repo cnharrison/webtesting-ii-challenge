abd14af538fa8b00c3c1003aff66e50327f7e84a
Christopher Harrison <cnharrison@gmail.com>
2019-06-26 18:19:05 -0400

    ball/strike/foul/hit lolgic

--- client/src/App.js
+++ client/src/App.js
@@ -11,17 +11,27 @@ class App extends Component {
 
   increment = type => {
     switch (true) {
+      case (type === "strike" && this.state.strikes === 2) ||
+        (type === "ball" && this.state.balls === 3):
+        this.setState({ balls: 0, strikes: 0 });
+        break;
       case type === "ball":
         this.setState({ balls: this.state.balls + 1 });
         break;
       case type === "strike":
         this.setState({ strikes: this.state.strikes + 1 });
         break;
-      case type === "foul":
+      case type === "foul" && this.state.strikes === 2:
         this.setState({ fouls: this.state.fouls + 1 });
         break;
+      case type === "foul" && this.state.strikes < 2:
+        this.setState({
+          fouls: this.state.fouls + 1,
+          strikes: this.state.strikes + 1
+        });
+        break;
       case type === "hit":
-        this.setState({ hits: this.state.hits + 1 });
+        this.setState({ hits: this.state.hits + 1, balls: 0, strikes: 0 });
         break;
       default:
         return;
